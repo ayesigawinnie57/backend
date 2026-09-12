d8ad9b056f6da35043fe4c1d8610e76780331066
Namatashabira <ssematasabira24@gmail.com>
2026-09-12 10:34:52 +0300

    Clear categories cache on admin save/delete

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.core.cache import cache
 from .models import Category, Product, ProductReview, ReviewImage
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        cache.delete('categories:list')
+
+    def delete_model(self, request, obj):
+        super().delete_model(request, obj)
+        cache.delete('categories:list')
+
+
 admin.site.register(Product)
 admin.site.register(ProductReview)
 admin.site.register(ReviewImage)
